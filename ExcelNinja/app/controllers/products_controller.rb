ba5ec9dd038bc19b43ae957d4a542d2cd2da7118
John T Cronin <jt.cronin@outlook.com>
2017-10-16 12:37:34 -0500

    trying to add headers

--- ExcelNinja/app/controllers/products_controller.rb
+++ ExcelNinja/app/controllers/products_controller.rb
@@ -1,9 +1,17 @@
 class ProductsController < ApplicationController
 	def index
-	  @products = Product.order('created_at DESC')
-	  respond_to do |format|
-	    format.html
-	    format.xlsx
-	  end
+	  # @products = Product.order('created_at DESC')
+	  # respond_to do |format|
+	  #   format.html
+	  #   format.xlsx
+	  # end
+
+	  @products = Prodcut.all
+
+    	respond_to do |format|
+        …
+        format.xlsx {
+            send_data Post.to_xlsx.to_stream.read, :filename => 'posts.xlsx', :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
+        }
 	end
 end
